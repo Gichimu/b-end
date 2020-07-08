c8ed5c4021d8db068f485028d0bdde2468bcd271
Gichimu <gichimueric12@gmail.com>
2020-07-08 21:41:05 +0300

    add static files

--- admin/settings.py
+++ admin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vmf)ey$=&s4g!m53rg&z4f_%u&li(tr!((rs%y8^&83u*&#wpj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
