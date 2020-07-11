f8178094861fad38db6b9719e78feccfb2b1185a
Gichimu <gichimueric12@gmail.com>
2020-07-11 13:04:26 +0300

    add admin section, navigation and css

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def properties(request):
+    return render(request, 'property.html')
